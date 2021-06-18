87b4d9248943f8ecfdaca6fe0ff5cc50c0ac84a1
Himanshu Dabas <20219940+himanshudabas@users.noreply.github.com>
2021-06-18 20:45:34 +0530

    change production api url
    update the url for backend api for production profile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://travel-ticketing.herokuapp.com',
   covidApiUrl: 'https://api.covid19api.com',
 };
